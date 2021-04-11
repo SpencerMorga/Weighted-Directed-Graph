d4c1448eca0ae03364bc2454df4dd8bff81d99a3
Spencer Morga <Spencer.Morga@GMR.local>
2021-04-11 14:32:59 -0700

    started weighteddirected, started functions, make getedge and go on from there

---
+++ Weighted Directed Graph/Weighted Directed Graph/Graph.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weighted_Directed_Graph
+{
+    public class Graph<T>
+    {
+        private List<Vertex<T>> verticies;
+        public IReadOnlyList<Vertex<T>> Verticies => verticies;
+        public IReadOnlyList<Edge<T>> Edges
+        {
+            get
+            {
+            }
+        }
+
+        public void AddVertex(Vertex<T> vertex)
+        {
+
+        }
+
+        public bool RemoveVertex(Vertex<T> vertex)
+        {
+
+        }
+
+        public bool AddEdge(Vertex<T> vertexA, Vertex<T> vertexB, double distance)
+        {
+
+        }
+
+        public bool RemoveEdge(Vertex<T> vertexA, Vertex<T> vertexB)
+        {
+
+        }
+
+        public Vertex<T> Search(Vertex<T> vertex)
+        {
+
+        }
+    }
+
+
+}
---
+++ Weighted Directed Graph/Weighted Directed Graph/Vertex.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weighted_Directed_Graph
+{
+    class Vertex
+    {
+    }
+}
